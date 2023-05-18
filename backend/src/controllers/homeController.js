a0a37c3a473e0a5bb907761c1921eca8b3932500
EpsilonNguyen <apdinh.nguyenductinh@gmail.com>
2023-05-18 15:43:55 +0700

    update frontend and backend

---
+++ backend/src/controllers/homeController.js
@@ -0,0 +1,34 @@
+import homeService from "../services/homeService";
+
+let getHome = (req, res) => {
+    return res.render("home.ejs");
+}
+
+let handleLogin = async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing inputs paramenter!'
+        })
+    }
+
+    let userData = await homeService.handleUserLogin(email, password);
+
+    //check email exist
+    //compare password
+    //return userInfor
+    //access_token: JWT json web token
+    return res.status(200).json({
+        errCode: userData.errCode,
+        errMessage: userData.errMessage,
+        user: userData.user ? userData.user : {}
+    })
+}
+
+module.exports = {
+    getHome: getHome,
+    handleLogin: handleLogin,
+}
